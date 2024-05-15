3c56c9ab4748549fe3d2c1a35acfee371ec9ef1e
Yulang Luo <luoyulan@betzgrp-wintermute.eecg.utoronto.ca>
2024-05-14 23:07:22 -0400

    [Testing] updating get_intermediate_file script
    1. The file now takes test suite name as input
    2. It outputs files to testing/tests dir
    3. testing/tests and testing/configs ignored

--- testing/README.md
+++ testing/README.md
@@ -1,4 +1,4 @@
 # get_intermediate_file.py manual
-Step 1. Move a run### directory or run_vtr_task's config directory into the directory containing the script.
+Step 1: Move a config file into the `configs` directory and name it `test_suite_config.txt`
 
-Step 2. `./fix_io.py circuit.v arch_desc.xml task_name`. If the run### exists, script prints the path to the directory containing all intermediate files. If the run### does not exist, run_vtr_task.py is called to run task named task_name and prints the path. 
+Step 2: Run the script with test suite name, the result will be located in `output_path/tests/suite_name` directory
--- testing/get_intermediate_file.py
+++ testing/get_intermediate_file.py
@@ -5,32 +5,37 @@ import argparse
 import os
 import subprocess
 import glob
+import shutil
 
 def parse_args(args):
     parser = argparse.ArgumentParser(description="parse arguments for fix io script")
-    parser.add_argument("test_case_name")
-    parser.add_argument("arch_desc_name")
-    parser.add_argument("task_name")
+    parser.add_argument("test_suite_name")
     default_vtr_path = os.path.expanduser("~") + "/vtr-verilog-to-routing"
     parser.add_argument("-vtr_path", default = default_vtr_path, type = str)
+    default_output_path = os.getcwd()+"/tests"
+    parser.add_argument("-output_path", default = default_output_path, type = str)
     return parser
 
 def run_test_main(args):
     args = parse_args(args).parse_args(args)
-    result_run_dir = glob.glob(os.getcwd()+"/run*/")
-    if len(result_run_dir) == 0:
-        if(not os.path.isdir("./config")):
-            print("missing config or run### dir")
-            return
-        result = subprocess.run([os.path.join(args.vtr_path, "vtr_flow/scripts/run_vtr_task.py"), "./"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        if(result.returncode != 0):
-            print("run_vtr_task failed: ", result.stderr)
-    result_run_dir = glob.glob(os.getcwd()+"/run*/")
-    result_dir = os.path.join(os.path.join(result_run_dir[0], args.arch_desc_name), args.test_case_name)
-    if(os.path.isdir(result_dir)):
-        print(result_dir)
-    else:
-        print("testcase architecture pair does not exist!")
+    if(not os.path.isdir(args.output_path + "/" + args.test_suite_name)):
+        os.makedirs(args.output_path + "/" + args.test_suite_name)
+    os.makedirs(os.getcwd()+"/tmp/")
+    if(not os.path.isfile(os.getcwd()+"/configs/"+args.test_suite_name+"_config.txt")):
+        print("config file for test suite " + args.test_suite_name + " does not exist!")
+        return
+    os.makedirs(os.getcwd()+"/tmp/config")
+    shutil.copy(os.getcwd()+"/configs/"+args.test_suite_name+"_config.txt", os.getcwd()+"/tmp/config/config.txt")
+    os.chdir(os.getcwd()+"/tmp/")
+    result = subprocess.run([os.path.join(args.vtr_path, "vtr_flow/scripts/run_vtr_task.py"), "./"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if(result.returncode != 0):
+        print("run_vtr_task failed: ", result.stderr)
+    os.chdir("../")
+    for item in os.listdir(os.getcwd()+"/tmp/run001"):
+        item_path = os.path.join(os.getcwd()+"/tmp/run001", item)
+        if(os.path.isdir(item_path) and item.endswith(".xml")):
+            shutil.move(item_path, args.output_path + "/" + args.test_suite_name)
+    shutil.rmtree(os.getcwd()+"/tmp")
 
 if __name__ == "__main__":
     run_test_main(sys.argv[1:])
